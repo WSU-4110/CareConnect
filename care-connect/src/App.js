c139bc7e6cab75d311214b7b97e9c67fa77cf2b4
burahaun <burahaunuddin@gmail.com>
2023-09-26 19:33:32 -0400

    Setup Care Connect UI with React and Added Landing Page
    Notes:
    * Setup the Care Connect front end using React
    * Added a components folder with initial LandingPage and edited App.js to render Landing Page
    * Updated .gitignore
    
    Testing:
    * npm start

---
+++ care-connect/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './components/LandingPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <LandingPage></LandingPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ care-connect/src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Care Connect</h1>
+      <h2>Care and Connect for a Healthier Tomorrow.</h2>
+      <div className="buttons">
+        <button className="login-button">Login</button>
+        <button className="signup-button">Signup</button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
