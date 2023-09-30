c849445610f2924630c91c97a635727feee931c3
Prangon108 <talukdarprangon108@gmail.com>
2023-09-30 13:39:52 -0400

    signup page added

--- care-connect/src/App.js
+++ care-connect/src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
-
-
+import Signup from './components/Signup'; // Import the Signup component
 
 function App() {
+  // Define a state variable to manage the visibility of the signup form
+  const [showSignup, setShowSignup] = useState(false);
+
+  // Function to toggle the signup form's visibility
+  const toggleSignup = () => {
+    setShowSignup(!showSignup);
+  };
+
   return (
     <div className="App">
-     <LandingPage></LandingPage>
+      <LandingPage>
+        {/* Render the Signup component conditionally */}
+        {showSignup ? (
+          <Signup />
+        ) : (
+          <div className="buttons">
+            <button className="login-button">Login</button>
+            <button className="signup-button" onClick={toggleSignup}>
+              Signup
+            </button>
+          </div>
+        )}
+      </LandingPage>
     </div>
   );
 }
--- care-connect/src/components/LandingPage.jsx
+++ care-connect/src/components/LandingPage.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Signup from './Signup'; // Import the Signup component
 
 const LandingPage = () => {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const toggleSignup = () => {
+    setShowSignup(!showSignup);
+  };
+
   return (
     <div className="landing-page">
       <h1>Welcome to Care Connect</h1>
       <h2>Care and Connect for a Healthier Tomorrow.</h2>
       <div className="buttons">
         <button className="login-button">Login</button>
-        <button className="signup-button">Signup</button>
+        <button className="signup-button" onClick={toggleSignup}>
+          Signup
+        </button>
       </div>
+      {showSignup && <Signup />}
     </div>
   );
 };
