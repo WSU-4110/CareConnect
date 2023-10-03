1f1a49fca559b8ba059014155ae033965bcf39a1
Prangon108 <talukdarprangon108@gmail.com>
2023-10-03 02:32:20 -0400

    modified Signup page

--- care-connect/src/App.js
+++ care-connect/src/App.js
@@ -1,34 +1,38 @@
 import React, { useState } from 'react';
+import Signup from './components/Signup';
 import './App.css';
-import LandingPage from './components/LandingPage';
-import Signup from './components/Signup'; // Import the Signup component
 
-function App() {
-  // Define a state variable to manage the visibility of the signup form
+const App = () => {
   const [showSignup, setShowSignup] = useState(false);
 
-  // Function to toggle the signup form's visibility
   const toggleSignup = () => {
     setShowSignup(!showSignup);
   };
 
   return (
-    <div className="App">
-      <LandingPage>
-        {/* Render the Signup component conditionally */}
-        {showSignup ? (
-          <Signup />
-        ) : (
-          <div className="buttons">
-            <button className="login-button">Login</button>
-            <button className="signup-button" onClick={toggleSignup}>
-              Signup
-            </button>
-          </div>
-        )}
-      </LandingPage>
+    <div className="app">
+      {showSignup ? (
+        <Signup />
+      ) : (
+        <LandingPage toggleSignup={toggleSignup} />
+      )}
     </div>
   );
-}
+};
+
+const LandingPage = ({ toggleSignup }) => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Care Connect</h1>
+      <h2>Care and Connect for a Healthier Tomorrow.</h2>
+      <div className="buttons">
+        <button className="login-button">Login</button>
+        <button className="signup-button" onClick={toggleSignup}>
+          Signup
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
