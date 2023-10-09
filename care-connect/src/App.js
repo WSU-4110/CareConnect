6e74c1cb8322e303a2fd7515638805a31ee9d843
Sabbir999 <srsabbbir206@gmail.com>
2023-10-09 15:54:58 -0400

    added Login page

--- care-connect/src/App.js
+++ care-connect/src/App.js
@@ -1,32 +1,44 @@
 import React, { useState } from 'react';
 import Signup from './components/Signup';
+import Login from './components/Login'; // Make sure you have a Login component at this path
 import './App.css';
 
 const App = () => {
   const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   const toggleSignup = () => {
     setShowSignup(!showSignup);
+    setShowLogin(false);
+  };
+
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+    setShowSignup(false);
   };
 
   return (
     <div className="app">
       {showSignup ? (
         <Signup />
+      ) : showLogin ? (
+        <Login />
       ) : (
-        <LandingPage toggleSignup={toggleSignup} />
+        <LandingPage toggleSignup={toggleSignup} toggleLogin={toggleLogin} />
       )}
     </div>
   );
 };
 
-const LandingPage = ({ toggleSignup }) => {
+const LandingPage = ({ toggleSignup, toggleLogin }) => {
   return (
     <div className="landing-page">
       <h1>Welcome to Care Connect</h1>
       <h2>Care and Connect for a Healthier Tomorrow.</h2>
       <div className="buttons">
-        <button className="login-button">Login</button>
+        <button className="login-button" onClick={toggleLogin}>
+          Login
+        </button>
         <button className="signup-button" onClick={toggleSignup}>
           Signup
         </button>
--- care-connect/src/components/LandingPage.jsx
+++ care-connect/src/components/LandingPage.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import Signup from './Signup'; // Import the Signup component
+import Login from './Login';  // Corrected the import
 
 const LandingPage = () => {
   const [showSignup, setShowSignup] = useState(false);
+  const [showLogin, setShowLogin] = useState(false); // Added this state
 
   const toggleSignup = () => {
     setShowSignup(!showSignup);
+    setShowLogin(false);  // Make sure the Login doesn't show when Signup is toggled
+  };
+
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+    setShowSignup(false); // Make sure the Signup doesn't show when Login is toggled
   };
 
   return (
@@ -13,12 +21,13 @@ const LandingPage = () => {
       <h1>Welcome to Care Connect</h1>
       <h2>Care and Connect for a Healthier Tomorrow.</h2>
       <div className="buttons">
-        <button className="login-button">Login</button>
+        <button className="login-button" onClick={toggleLogin}>Login</button>
         <button className="signup-button" onClick={toggleSignup}>
           Signup
         </button>
       </div>
       {showSignup && <Signup />}
+      {showLogin && <Login />}
     </div>
   );
 };
