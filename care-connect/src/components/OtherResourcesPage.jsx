b9725f505ffbfad556f9bb0cb7871b61dad25d50
Lucas Prifti <144178397+LucasPrifti@users.noreply.github.com>
2023-10-31 11:15:38 -0400

    FACTORY DESIGN PATTERN
    Assignment 4

--- care-connect/src/components/OtherResourcesPage.jsx
+++ care-connect/src/components/OtherResourcesPage.jsx
@@ -1,43 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './OtherResourcesPage.css'; // Import the CSS file
+import './OtherResourcesPage.css';
 
-const otherResourcesData = [
-  {
-    title: 'BLOG: How to support someone with a Mental Health Problem',
-    thumbnail: './ResourceLibraryImages/resource1.jpg',
-    link: 'https://www.mentalhealth.org.uk/explore-mental-health/articles/how-support-someone-mental-health-problem',
-  },
-  {
-    title: 'Study on Talk Therapy for Depression',
-    thumbnail: './ResourceLibraryImages/resource2.avif',
-    link: 'https://www.health.harvard.edu/heart-health/talk-therapy-for-depression-may-help-lower-heart-disease-risk',
-  },
+class ResourceCard {
+  constructor(title, thumbnail, link) {
+    this.title = title;
+    this.thumbnail = thumbnail;
+    this.link = link;
+  }
 
-  // Will add more resource data as needed
-];
+  create() {
+    return (
+      <div className="resource-card">
+        <img src={this.thumbnail} alt={this.title} />
+        <h2>{this.title}</h2>
+        <a
+          href={this.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="resource-link"
+        >
+          View Resource
+        </a>
+      </div>
+    );
+  }
+}
 
 const OtherResourcesPage = () => {
+  const otherResourcesData = [
+    new ResourceCard(
+      'BLOG: How to support someone with a Mental Health Problem',
+      '/ResourceLibraryImages/resource1.jpg',
+      'https://www.mentalhealth.org.uk/explore-mental-health/articles/how-support-someone-mental-health-problem'
+    ),
+    new ResourceCard(
+      'Study on Talk Therapy for Depression',
+      '/ResourceLibraryImages/resource2.avif',
+      'https://www.health.harvard.edu/heart-health/talk-therapy-for-depression-may-help-lower-heart-disease-risk'
+    ),
+    // Add more resource data as needed...
+  ];
+
   return (
     <div className="other-resources-page">
       <h1>Other Resources</h1>
       <div className="resource-list">
-      <Link to="/" className="go-back-button">
-      Back to HomePage
-      </Link>
+        <Link to="/" className="go-back-button">
+          Back to HomePage
+        </Link>
         {otherResourcesData.map((resource, index) => (
-          <div key={index} className="resource-card">
-            <img src={resource.thumbnail} alt={`Resource ${index + 1}`} />
-            <h2>{resource.title}</h2>
-            <a
-              href={resource.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="resource-link"
-            >
-              View Resource
-            </a>
-          </div>
+          <div key={index}>{resource.create()}</div>
         ))}
       </div>
     </div>
