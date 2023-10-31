a95630893f13c7f3f405f2b60d82d6c83bab52ed
Saifur Sabbir <69662783+Sabbir999@users.noreply.github.com>
2023-10-31 01:24:10 -0400

    Implemented Strategy pattern for login

--- client/src/components/LocalAuthStrategy.js
+++ client/src/components/LocalAuthStrategy.js
@@ -2,7 +2,6 @@ import AuthStrategy from "./AuthStrategy";
 
 class LocalAuthStrategy extends AuthStrategy {
   authenticate(data) {
-    // Implement local authentication logic here
     return axios.post("http://localhost:8080/api/auth/local", data);
   }
 }
