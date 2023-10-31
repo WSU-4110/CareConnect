e7a68415408bb9dd6324d6b7720a9a0bb53b8da0
Lucas Prifti <144178397+LucasPrifti@users.noreply.github.com>
2023-10-31 11:12:19 -0400

    Update ArticlesPage.jsx
    Assignment 4: Factory Design Pattern

--- care-connect/src/components/ArticlesPage.jsx
+++ care-connect/src/components/ArticlesPage.jsx
@@ -1,53 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './ArticlesPage.css'; // Import the CSS file 
+import './ArticlesPage.css';
 
+class ArticleCard {
+  constructor(image, title, description, link) {
+    this.image = image;
+    this.title = title;
+    this.description = description;
+    this.link = link;
+  }
 
-
+  create() {
+    return (
+      <div className="article-card">
+        <img src={this.image} alt={this.title} />
+        <h2>{this.title}</h2>
+        {this.description && <h3>{this.description}</h3>}
+        <a
+          href={this.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="read-button"
+        >
+          Read Article
+        </a>
+      </div>
+    );
+  }
+}
 
 const ArticlesPage = () => {
   const articlesData = [
-    {
-      image: '/ResourceLibraryImages/article1.jpg', 
-      title: 'The Importance of Mental Health Awareness', 
-      description: 'Learn why your Mental Health is so Important',
-      link: 'https://www.pinerest.org/newsroom/articles/mental-health-awareness-blog/', 
-    },
-    {
-      image: '/ResourceLibraryImages/article2.jpg', 
-      title: 'Stress Management: How to Reduce and Relieve Stress', 
-      link: 'https://www.helpguide.org/articles/stress/stress-management.htm', 
-    },
-    {
-      image: '/ResourceLibraryImages/article3.jpg', 
-      title: 'Building Better Mental Health', 
-      description: 'Learn how to Fortify your Mental Health...',
-      link: 'https://www.helpguide.org/articles/mental-health/building-better-mental-health.htm#:~:text=Your%20mental%20health%20influences%20how,from%20life%27s%20setbacks%20and%20hardships.', 
-    },
-    // Will add more articles as needed...
+    new ArticleCard(
+      '/ResourceLibraryImages/article1.jpg',
+      'The Importance of Mental Health Awareness',
+      'Learn why your Mental Health is so Important',
+      'https://www.pinerest.org/newsroom/articles/mental-health-awareness-blog/'
+    ),
+    new ArticleCard(
+      '/ResourceLibraryImages/article2.jpg',
+      'Stress Management: How to Reduce and Relieve Stress',
+      null,
+      'https://www.helpguide.org/articles/stress/stress-management.htm'
+    ),
+    new ArticleCard(
+      '/ResourceLibraryImages/article3.jpg',
+      'Building Better Mental Health',
+      'Learn how to Fortify your Mental Health...',
+      'https://www.helpguide.org/articles/mental-health/building-better-mental-health.htm#:~:text=Your%20mental%20health%20influences%20how,from%20life%27s%20setbacks%20and%20hardships.'
+    ),
+    // Add more articles as needed...
   ];
 
   return (
     <div className="article-page">
       <h1>Articles</h1>
       <div className="article-list">
-      <Link to="/" className="go-back-button">
-        Back to HomePage
-      </Link>
+        <Link to="/" className="go-back-button">
+          Back to HomePage
+        </Link>
         {articlesData.map((article, index) => (
-          <div key={index} className="article-card">
-            <img src={article.image} alt={`Article ${index + 1}`} />
-            <h2>{article.title}</h2>
-            <h3>{article.description}</h3>
-            <a
-              href={article.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="read-button"
-            >
-              Read Article
-            </a>
-          </div>
+          <div key={index}>{article.create()}</div>
         ))}
       </div>
     </div>
