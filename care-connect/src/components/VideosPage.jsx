d072fd026c0594c842ad3f840ebfc367e4d242f1
Lucas Prifti <144178397+LucasPrifti@users.noreply.github.com>
2023-10-31 11:16:49 -0400

    FACTORY DESIGN PATTERN
    Assignment 4

--- care-connect/src/components/VideosPage.jsx
+++ care-connect/src/components/VideosPage.jsx
@@ -1,47 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './VideosPage.css'; // Import the CSS file
+import './VideosPage.css';
 
-const videosData = [
-  {
-    title: 'How to release Stress and Overthinking',
-    thumbnail: './ResourceLibraryImages/video1.jpg',
-    link: 'https://www.youtube.com/watch?v=RCheYNZN88k',
-  },
-  {
-    title: 'Inspiring Speech on Mental Health',
-    thumbnail: './ResourceLibraryImages/video2.jpg',
-    link: 'https://www.youtube.com/watch?v=2NZDp0E-HFc',
-  },
-  {
-    title: 'Rebuild Yourself',
-    thumbnail: './ResourceLibraryImages/video3.jpg',
-    link: 'https://www.youtube.com/watch?v=6OLhDIpvu9w',
-  },
-  // Will add more video data as needed
-];
+class VideoCard {
+  constructor(title, thumbnail, link) {
+    this.title = title;
+    this.thumbnail = thumbnail;
+    this.link = link;
+  }
+
+  create() {
+    return (
+      <div className="video-card">
+        <img src={this.thumbnail} alt={this.title} />
+        <h2>{this.title}</h2>
+        <a
+          href={this.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="watch-button"
+        >
+          Watch Video
+        </a>
+      </div>
+    );
+  }
+}
 
 const VideosPage = () => {
+  const videosData = [
+    new VideoCard(
+      'How to release Stress and Overthinking',
+      '/ResourceLibraryImages/video1.jpg',
+      'https://www.youtube.com/watch?v=RCheYNZN88k'
+    ),
+    new VideoCard(
+      'Inspiring Speech on Mental Health',
+      '/ResourceLibraryImages/video2.jpg',
+      'https://www.youtube.com/watch?v=2NZDp0E-HFc'
+    ),
+    new VideoCard(
+      'Rebuild Yourself',
+      '/ResourceLibraryImages/video3.jpg',
+      'https://www.youtube.com/watch?v=6OLhDIpvu9w'
+    ),
+    // Add more video data as needed...
+  ];
+
   return (
     <div className="video-page">
       <h1>Videos</h1>
       <div className="video-list">
-      <Link to="/" className="go-back-button">
-      Back to HomePage
-      </Link>
+        <Link to="/" className="go-back-button">
+          Back to HomePage
+        </Link>
         {videosData.map((video, index) => (
-          <div key={index} className="video-card">
-            <img src={video.thumbnail} alt={`Video ${index + 1}`} />
-            <h2>{video.title}</h2>
-            <a
-              href={video.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="watch-button"
-            >
-              Watch Video
-            </a>
-          </div>
+          <div key={index}>{video.create()}</div>
         ))}
       </div>
     </div>
