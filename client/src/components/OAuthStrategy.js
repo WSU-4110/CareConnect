098ccf131ab4c37a9c658ef472d696a2616a2c6b
Saifur Sabbir <69662783+Sabbir999@users.noreply.github.com>
2023-10-31 01:24:48 -0400

    Implemented Strategy pattern for login

--- client/src/components/OAuthStrategy.js
+++ client/src/components/OAuthStrategy.js
@@ -2,7 +2,6 @@ import AuthStrategy from "./AuthStrategy";
 
 class OAuthStrategy extends AuthStrategy {
   authenticate(data) {
-    // Implement OAuth authentication logic here
     return axios.post("http://localhost:8080/api/auth/oauth", data);
   }
 }
