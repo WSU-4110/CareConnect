83c973f62ceb5cc28c28df7db1f35d2843253352
Mohan Sreeramulu <hi1396@wayne.edu>
2023-11-04 15:57:35 -0400

    Added Profile page template

--- client/src/components/ViewProfile/index.jsx
+++ client/src/components/ViewProfile/index.jsx
@@ -8,7 +8,71 @@ const ViewProfile = () => {
         <div className= {styles["left-section"]}>
         
 
-<h1>Profile Page</h1>
+<div className={styles.right}>
+					<form className={styles.form_container} >
+						<h1>User Profile</h1>
+						<input
+							type="text"
+							placeholder="First Name"
+							name="firstName"
+							required
+							className={styles.input}
+						/>
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="lastName"
+							required
+							className={styles.input}
+						/>
+						<input
+							type="email"
+							placeholder="Email"
+							name="email"
+							required
+							className={styles.input}
+						/>
+						<input
+							type="Gender"
+							placeholder="Gender"
+							name="Gender"
+							required
+							className={styles.input}
+						/>
+                        <input
+							type="DOB"
+							placeholder="DOB"
+							name="DOB"
+							required
+							className={styles.input}
+						/>
+
+                        <input
+							type="Profession"
+							placeholder="Profession"
+							name="Profession"
+							required
+							className={styles.input}
+						/>
+
+                        <input
+							type="About Me"
+							placeholder="About Me"
+							name="About Me"
+							required
+							className={styles.input}
+						/>
+						
+                        <div>
+						<button type="submit" className={styles.green_btn}>
+							Save
+						</button>
+                        <button type="submit" className={styles.green_btn}>
+							Cancel
+						</button>
+                        </div>
+					</form>
+				</div>
 
 </div>
 
